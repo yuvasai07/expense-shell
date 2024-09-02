0e4f25fcff7db23a3beaa964018721267a0c6bea
yuvasai07 <yuvasaimalapati@gmail.com>
2024-09-02 22:42:40 +0530

    i am automating mysql

--- mysql.sh
+++ mysql.sh
@@ -43,7 +43,7 @@ VALIDATE $? "Enabled MySQL Server"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Started MySQL server"
 
-mysql -h mysql.daws81s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+mysql -h 54.84.52.113 -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
